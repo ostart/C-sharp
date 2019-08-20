a12a121690e136476c7f31db2eb7eaad64dc7162
Artem Osta <ostart@mail.ru>
2019-08-20 08:08:15 +0300

    aBST has done

---
+++ Tests_aBST.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using AlgorithmsDataStructures2;
+
+namespace Tests
+{
+    class Tests_aBST
+    {
+        private aBST _tree;
+
+        [SetUp]
+        public void TestsSetup()
+        {
+            _tree = new aBST(3);
+            _tree.Tree = new int?[] {50, 25, 75, null, 37, 62, 84, null, null, 31, 43, 55, null, null, 92};
+        }
+
+        [TearDown]
+        public void TestsTeardown()
+        {
+            _tree = null;
+        }
+
+        [Test]
+        public void TestFindKeyIndex()
+        {
+            Assert.AreEqual(0, _tree.FindKeyIndex(50));
+            Assert.AreEqual(1, _tree.FindKeyIndex(25));
+            Assert.AreEqual(2, _tree.FindKeyIndex(75));
+            Assert.AreEqual(4, _tree.FindKeyIndex(37));
+            Assert.AreEqual(5, _tree.FindKeyIndex(62));
+            Assert.AreEqual(6, _tree.FindKeyIndex(84));
+            Assert.AreEqual(9, _tree.FindKeyIndex(31));
+            Assert.AreEqual(10, _tree.FindKeyIndex(43));
+            Assert.AreEqual(11, _tree.FindKeyIndex(55));
+            Assert.AreEqual(14, _tree.FindKeyIndex(92));
+
+            Assert.AreEqual(-3, _tree.FindKeyIndex(24));
+            Assert.AreEqual(-12, _tree.FindKeyIndex(63));
+            Assert.AreEqual(-13, _tree.FindKeyIndex(83));
+        }
+
+        [Test]
+        public void TestAddKey()
+        {
+            Assert.AreEqual(-1, _tree.AddKey(50));
+            Assert.AreEqual(-1, _tree.AddKey(25));
+            Assert.AreEqual(-1, _tree.AddKey(62));
+            Assert.AreEqual(-1, _tree.AddKey(92));
+            Assert.AreEqual(3, _tree.AddKey(24));
+            Assert.AreEqual(13, _tree.AddKey(83));
+        }
+    }
+}
---
+++ aBST.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+  public class aBST
+  {
+    public int? [] Tree; // массив ключей
+	
+    public aBST(int depth)
+    {
+      // правильно рассчитайте размер массива для дерева глубины depth:
+      int tree_size = 0;
+      for(int i=0; i<=depth; i++) tree_size += 2 ^ i;
+      Tree = new int?[ tree_size ];
+      for(int i=0; i<tree_size; i++) Tree[i] = null;
+    }
+	
+    public int? FindKeyIndex(int key)
+    {
+      // ищем в массиве индекс ключа
+      return GetIndex(0, key);
+    }
+
+    private int? GetIndex(int currentIndex, int key)
+    {
+        if(currentIndex > Tree.Length) return null;
+        var currentValue = Tree[currentIndex];
+        if(currentValue == null) return -1 * currentIndex;
+        if(currentValue < key)
+            return GetIndex(2 * currentIndex + 2, key);
+        else if (currentValue > key)
+            return GetIndex(2 * currentIndex + 1, key);
+        else
+            return currentIndex;
+    }
+
+    public int AddKey(int key)
+    {
+      // добавляем ключ в массив
+      var index = FindKeyIndex(key);
+      if(index == null || index > 0) return -1;
+      if(index == 0) 
+      {
+          var currentValue = Tree[0];
+          if(currentValue != null) return -1;
+      }
+      // индекс добавленного/существующего ключа или -1 если не удалось
+      var resultIndex = (int) (-1 * index);
+      Tree[resultIndex] = key;
+      return resultIndex;
+    }	
+  }
+}
